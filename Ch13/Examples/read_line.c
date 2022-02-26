2b709628cf15888cdd36cc80b6afd721ba68fbe3
lady <jhdbui@gmail.om>
2022-02-26 11:45:00 -0800

    Added change from Q&A

--- Ch13/Examples/read_line.c
+++ Ch13/Examples/read_line.c
@@ -6,7 +6,7 @@ int read_line(char str[], int n)
 {
     int ch, i = 0;
 
-    while ((ch = getchar()) != '\n')
+    while ((ch = getchar()) != '\n' && ch != EOF)
         if (i < n)
             str[i++] = ch;
     str[i] = '\0';      /* terminates string */
